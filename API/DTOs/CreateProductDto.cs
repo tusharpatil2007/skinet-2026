d22d82db763ef481351d9c23c4d65f7b1a759fa1
tusharpatil2007 <tushar.patil2007@gmail.com>
2026-08-02 17:46:29 +0530

    End of Sectrion 6

---
+++ API/DTOs/CreateProductDto.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+public class CreateProductDto
+{
+    [Required]
+    public  string Name { get; set; } = string.Empty;
+
+    [Required]
+    public string Description {get; set;} = string.Empty;
+
+    [Range(0.01, double.MaxValue,ErrorMessage ="Price Must be grater than 0")]
+    public decimal Price { get; set; }
+
+    [Required]
+    public string PictureUrl { get; set; } = string.Empty;
+
+    [Required]
+    public string Type { get; set; } = string.Empty;
+
+    [Required]
+    public string Brand { get; set; } = string.Empty;
+
+    [Range(1, int.MaxValue,ErrorMessage ="Quantity in stock must be at least 1")]
+    public int QuantityStock { get; set; }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,17 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 
 builder.Services.AddScoped<IProductRepository,ProductRepository>();
-
 builder.Services.AddScoped(typeof(IgenericRepository<>), typeof (GenericRepository<>));
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+//Configure the HTTP request pipeline
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
